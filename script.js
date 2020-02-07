78c0ab32ffcd812c32d7a37ca4451530efab773f
Brennan Walsh <56426721+bjw3a@users.noreply.github.com>
2020-02-06 21:53:24 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function runCode() {
+    alert ('hey there!! just making sure this works!!');
+}
+
+
+    
+
+
+
+function booBoo() {
+    var boo = prompt ("are you a boy or girl?"); 
+    {alert ('you bad baby');} } 
+    
+
+
+
+
+function bigBoy(){
+    var blue = prompt("what is your favorite color"); if(blue=='blue') {
+        alert('you have chosen correctly sir! or mam!');} 
+        else{ alert('that is a dumb color and I think you should re think your choice');}
+    
+}
+function badDad() {
+    var dad = prompt ("Do you like me?"); if (dad== 'yes') { 
+        alert ('good, because I really really like you bro');}
+        else{ alert ('fuck you!');}
+
+    
+}
+
+function runCode2(){
+    var tree = prompt('type yes or no'); if (tree=='yes') {alert ('yes yes yes'); }
+    else {alert(' no no no')}
+}
+
+function goBaby(){
+    var A = prompt ('do you like to eat pee pee?'); if (A=='yes') {
+        alert ('well, good for you!');}
+     else { alert ('well, you are missing out')}
+}
+
+function heyBaby(){
+    alert('thank you')
+}
+function newCode(){
+    alert('just kidding click the submit button below')
+}
